9902fe564fc0f46df27f1096b5a7c7bd637c27f2
HOME-PC\Andrey <greemrock911@gmail.com>
2021-02-04 12:47:53 +0300

    feat: Dream team - done

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function createDreamTeam(members) {
+  let team = [];
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (members === null || members === undefined) return false;
+  else {
+    for (let i = 0; i < members.length; i++){
+      if (typeof members[i] == 'string') {
+        team.push(members[i].replace(/\s+/g, '').split(','));
+      }
+    }
+
+  }
+  return team.map(item => item[0][0]).join('').toUpperCase().split('').sort().join('');
 };
