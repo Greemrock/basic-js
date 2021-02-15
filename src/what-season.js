fc82962bb28edafe55d438e9482df511294bcee3
HOME-PC\Andrey <greemrock911@gmail.com>
2021-02-15 20:54:20 +0300

    fix: 1 failing - done

--- src/what-season.js
+++ src/what-season.js
@@ -1,8 +1,10 @@
 module.exports = function getSeason(date) {
-    if (date == null) return 'Unable to determine the time of year!';
+    if (!date) return 'Unable to determine the time of year!';
+    if (typeof date === 'string' || !date.getTime()) throw new Error();
     
-    if (date.getMonth() >= 0 &&  date.getMonth() < 2 || date.getMonth() === 11) return "winter";
-    if (date.getMonth() > 1 &&  date.getMonth() < 5) return "spring";
-    if (date.getMonth() > 4 &&  date.getMonth() < 8) return "summer";
-    if (date.getMonth() > 7 &&  date.getMonth() < 11) return "autumn";
+    let month = date.getMonth();
+    if (month < 2 || month > 10) return "winter";
+    if (month > 1 &&  month < 5) return "spring";
+    if (month > 4 &&  month < 8) return "summer";
+    if (month > 7 &&  month < 11) return "autumn";
 };
