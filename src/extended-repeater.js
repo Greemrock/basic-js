f87ffa462d75622cad19ceb2f3f2d6110c88ee0c
HOME-PC\Andrey <greemrock911@gmail.com>
2021-02-16 14:42:35 +0300

    feat: extend-repeater - done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,57 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  let arr =[];
+	let repeatTimes = options.repeatTimes;
+  if (repeatTimes == undefined) repeatTimes = 1;
+	if (str === null) str = "null";
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+	for (let i = 0; i < repeatTimes; i++) {
+		arr.push(str)
+	}
+
+	const finishRepiter = (arr) => {
+		let length = arr.length;
+		let separator = options.separator;
+		let addition = options.addition;
+
+		if (addition === null) addition = "null";
+		if (typeof addition == "boolean")	addition = addition.toString();
+
+		for (let i = 0; i < length; i++) {
+			if (addition && arr[i]) {
+				arr[i] += additionRepeat(options);
+			}		
+			if (separator && arr[i+1]) {
+				arr[i] += separator;
+			} else if(arr[i+1]) {
+				arr[i] += '+';
+			} 
+		}
+
+		return arr.join('');
+	}
+
+	return finishRepiter(arr);
 };
-  
+
+const additionRepeat = (options) => {
+	let additionArr = [];
+
+  if (options.addition === null) options.addition = "null";
+
+	let addition = options.addition.toString();
+	let additionRepeatTimes = options.additionRepeatTimes;
+	let additionSeparator = options.additionSeparator;
+
+	if (additionRepeatTimes == undefined) additionRepeatTimes = 1;
+
+	for (let i = 0; i < additionRepeatTimes; i++) {
+		additionArr.push(addition);	
+	}
+
+	for (let i = 0; i < additionRepeatTimes; i++) {
+		if(additionArr[i+1]) {
+			additionArr[i] += additionSeparator;
+		}
+	}
+	return additionArr.join('');
+}
